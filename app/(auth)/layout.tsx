1b76a14c47371335f96dbd553ee3ea37ff8b0f60
furkan eriç <furkaneric9@gmail.com>
2025-05-14 15:24:55 +0300

    Giriş cıkış işlemleri yapıldı

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,16 +1,47 @@
-import React from 'react'
-
+import { X } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
 interface AuthLayoutProps {
-    children: React.ReactNode
+  children: React.ReactNode;
 }
-const AuthLayout = ({children}:AuthLayoutProps) => {
+const AuthLayout = ({ children }: AuthLayoutProps) => {
   return (
-    <div className='flex gap-2 max-h-screen relative'>
-        
-      {children}
-    </div>
-  )
-}
+    <div className="flex gap-2 max-h-screen relative">
+      <div className="absolute top-2 left-2">
+        <Link href="/">
+          <X className="text-slate-700" />
+        </Link>
+      </div>
+      <div className="w-full  md:w-1/2 flex flex-col items-center justify-center h-screen">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            alt="logo"
+            width={512}
+            height={120}
+            className="h-10 w-auto"
+          />
+        </Link>
 
-export default AuthLayout
+        <div className="text-center">
+          <h2 className="font-semibold text-2xl">Welcome Back</h2>
+        </div>
+
+        {children}
+      </div>
+      <div className="hidden md:flex w-1/2 h-screen">
+        <Image
+          alt="auth"
+          src="/auth.jpg"
+          width={1080}
+          height={1920}
+          className="object-cover w-full h-full"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default AuthLayout;
--- app/(routes)/_components/Menu/UserMenu.tsx
+++ app/(routes)/_components/Menu/UserMenu.tsx
@@ -1,18 +1,33 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
+import { currentUser } from "@clerk/nextjs/server";
 import Link from "next/link";
 import React from "react";
 
-const UserMenu = () => {
+const UserMenu = async () => {
+  const user = await currentUser();
   return (
     <div>
-      <div className="space-x-4">
-        <Link href="/sing-in">
-          <Button variant={"mybutton"}>Login</Button>
-        </Link>
-        <Link href="/sign-up">
-          <Button variant="default">Sign Up</Button>
-        </Link>
-      </div>
+      {user && (
+        <>
+          <div className="flex flex-row items-center justify-center gap-3">
+            <Link href="/sign-in">
+              <Button variant="outline"> Dashboard</Button>
+            </Link>
+            <UserButton />
+          </div>
+        </>
+      )}
+      {!user && (
+        <div className="space-x-4">
+          <Link href="/sign-in">
+            <Button variant={"mybutton"}>Login</Button>
+          </Link>
+          <Link href="/sign-up">
+            <Button variant="default">Sign Up</Button>
+          </Link>
+        </div>
+      )}
     </div>
   );
 };
